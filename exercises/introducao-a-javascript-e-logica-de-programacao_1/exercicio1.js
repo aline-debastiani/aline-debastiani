491ac1d67d37116825a0aef833da7d1568a2bbb9
Aline Debastiani <debastianialine@gmail.com>
2020-10-23 19:24:05 -0300

    executa operacoes de aritmetica basica

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -0,0 +1,17 @@
+let a = 2;
+let b = 10;
+
+let soma = a + b;
+console.log(soma);
+
+let subtracao = a - b;
+console.log(subtracao);
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+let divisao = a / b;
+console.log(divisao);
+
+let modulo = a % b;
+console.log(modulo);
