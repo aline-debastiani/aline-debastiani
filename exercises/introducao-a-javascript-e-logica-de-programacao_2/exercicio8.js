ad4b0d2b6b90c4b3a7047ffb09b3eb81241af3c9
Aline Debastiani <debastianialine@gmail.com>
2020-10-26 19:21:36 -0300

    cumpre exercicio 8

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio8.js
@@ -0,0 +1,8 @@
+//Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = [];
+
+for (let index = 1; index <= 25; index++) {
+    numbers.push(index);
+}
+console.log(numbers);
