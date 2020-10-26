cb52fc8740913c9fa305f11f07584b7425e20e5d
Aline Debastiani <debastianialine@gmail.com>
2020-10-26 18:57:04 -0300

    cumpre exercicio 5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
@@ -0,0 +1,11 @@
+//Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor =0;
+
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] > maiorValor) {
+    maiorValor = numbers[index];
+}
+}
+console.log(maiorValor);
