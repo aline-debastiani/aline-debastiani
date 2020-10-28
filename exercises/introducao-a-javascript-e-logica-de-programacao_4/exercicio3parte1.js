b958fe830be1201d63fb35912d7236efb0123e67
Aline Debastiani <debastianialine@gmail.com>
2020-10-28 16:45:44 -0300

    cumpre exercicio 3 parte 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio3parte1.js
@@ -0,0 +1,13 @@
+//Faça um for/in que mostre todas as chaves do objeto.
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recorrente = "Sim";
+
+for (let key in info) {
+    console.log(key);
+}
