eadffa34e8e35da59e3b4d482207c713abb4da1a
Aline Debastiani <debastianialine@gmail.com>
2020-11-05 14:26:33 -0300

    cumpre exercicio 9

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -140,3 +140,17 @@ function newTaskDiv(color) {
     tasksContainer.appendChild(newTask);
 };
 newTaskDiv('blue');
+
+function setTaskClass() {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let myTasks = document.querySelector('.task')
+     
+    myTasks.addEventListener('click', function(event) {
+    if (selectedTask.length === 0) {
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  });
+};
+setTaskClass();
