e8e886a07641f26831cd0f68ab1e3df2d2a8837e
Aline Debastiani <debastianialine@gmail.com>
2020-11-05 03:02:51 -0300

    cumpre exercicio 6

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -100,3 +100,24 @@ function displayFridays(fridaysArray) {
 };
 let dezFridays = [4, 11, 18, 25];
 displayFridays(dezFridays);
+
+function dayMouseOver() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '30px';
+    event.target.style.fontWeight = '600';
+  })
+};
+
+function dayMouseOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '200';
+    event.target.style.fontSize = '20px';
+  })
+};
+
+dayMouseOver();
+dayMouseOut();
