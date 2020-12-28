038f45ee6e35e07494ecc6692b37a091e1fe2cb3
Aline Debastiani <debastianialine@gmail.com>
2020-12-28 15:13:36 -0300

    cumpre exercicio 2

--- exercises/javascript-assincrono-e-promises_2/apiScript.js
+++ exercises/javascript-assincrono-e-promises_2/apiScript.js
@@ -14,3 +14,22 @@ fetch(API_URL, myObject)
 };
 
 window.onload = () => fetchJoke();
+
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const myArray = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+    const sum = myArray.map(number => number * number)
+                       .reduce((number, acc) => number + acc, 0);
+
+    (sum < 8000) ? resolve() : reject();
+  });
+
+  myPromise
+    .then(() => console.log('Deu bom'))
+    .catch(() => console.log('Deu ruim'));
+};
+
+fetchPromise();
