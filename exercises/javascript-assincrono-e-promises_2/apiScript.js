2e95cbd89ba94136080e24b422ff0c1e6d60fe48
Aline Debastiani <debastianialine@gmail.com>
2020-12-28 15:08:13 -0300

    cumpre exercicio 1

---
+++ exercises/javascript-assincrono-e-promises_2/apiScript.js
@@ -0,0 +1,16 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
+fetch(API_URL, myObject)
+.then((response) => {
+  response.json().then((data) => {    
+    document.getElementById('jokeContainer').innerHTML = data.joke;
+  })
+})
+};
+
+window.onload = () => fetchJoke();
